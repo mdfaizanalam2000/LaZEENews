54b513ede0ecd58b6159d2f415288ab1ff2f4c2d
MD Faizan Alam <alamf015@gmail.com>
2025-12-14 01:28:53 +0530

    fixed cors for search also

--- src/components/Searched.js
+++ src/components/Searched.js
@@ -7,10 +7,8 @@ const Searched = (props) => {
     const [loading, setLoading] = useState(false);
 
     const update = async () => {
-        let url = `https://gnews.io/api/v4/search?q=${encodeURIComponent(props.category)}&apikey=b96948dc872709ab064f0987b5361479&lang=en&country=in`
-        setLoading(true);
         props.setProgress(20);
-        let data = await fetch(url);
+        let data = await fetch(`/.netlify/functions/news?category=${encodeURIComponent(props.category)}`);
         // let data = await fetch("./sample_data.json");
         props.setProgress(50);
         let parsedData = await data.json();
